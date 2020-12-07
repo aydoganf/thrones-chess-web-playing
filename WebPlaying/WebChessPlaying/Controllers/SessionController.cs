508b7b6b96c082522ca96df9d0132d23813f0d9e
Faruk Aydogan <aydoganf@itu.edu.tr>
2020-12-07 16:59:45 +0300

    alright

---
+++ WebPlaying/WebChessPlaying/Controllers/SessionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChessPlaying.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult View(Guid id, [FromQuery(Name = "nickname")] string nickname)
+        {
+            ViewBag.SessionName = id.ToString();
+            ViewBag.PlayerNickname = nickname;
+            return View();
+        }
+    }
+}
--- WebPlaying/WebChessPlaying/Startup.cs
+++ WebPlaying/WebChessPlaying/Startup.cs
@@ -46,6 +46,9 @@ namespace WebChessPlaying
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
